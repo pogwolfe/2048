aa9319421735cd78ca3b5dbe35fdfa2edcf357a0
katiestacey44 <117104997+katiestacey44@users.noreply.github.com>
2022-11-03 17:09:08 -0400

    new test classes

--- project3/Test2048.java
+++ project3/Test2048.java
@@ -11,9 +11,40 @@ public class Test2048 {
 
 
     }
+    @Test
+    public void testBoard2() {
+
+
+    }
+
 
     @Test(expected = IllegalArgumentException.class)
-    public void testBoard2() {
+    public void testBoard3() {
+
+
+    }
+    @Test
+    public void testBoard4() {
+
+
+    }
+    @Test
+    public void testTile1() {
+
+
+    }
+    @Test
+    public void testTile2() {
+
+
+    }
+    @Test
+    public void testTile3() {
+
+
+    }
+    @Test
+    public void testTile4() {
 
 
     }
@@ -61,6 +92,21 @@ public class Test2048 {
 
     @Test
     public void testgetValue12() {
+
+
+    }
+    @Test
+    public void testPower2() {
+
+
+    }
+    @Test
+    public void testtoString1() {
+
+
+    }
+    @Test
+    public void testtoString2() {
 
 
     }
