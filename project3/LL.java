6865147727a765c99a14f91288a98733a2321b32
Parkerkuch <117104975+Parkerkuch@users.noreply.github.com>
2022-11-04 12:34:04 -0400

    toArray method

--- project3/LL.java
+++ project3/LL.java
@@ -6,17 +6,17 @@ public class LL<E> { // still needs to be changed to fit 2048
     private Node<E> tail;
     private int size;
 
-    public LL(){ // uses other LinkedList constructor
+    public LL() { // uses other LinkedList constructor
         this(0);
     }
 
-    public LL(int initial){ // LinkedList constructor
-        if (initial != 0 && initial > 0){ // create the initial Node
+    public LL(int initial) { // LinkedList constructor
+        if (initial != 0 && initial > 0) { // create the initial Node
             head = new Node<E>();
             tail = head;
             size++;
             Node<E> current = head;
-            while(size < initial){ // create a new Node and have the last one point to it
+            while (size < initial) { // create a new Node and have the last one point to it
                 current.next = new Node<E>(); // new Node
                 current = current.next; // have the last one point to it
                 size++;
@@ -25,12 +25,12 @@ public class LL<E> { // still needs to be changed to fit 2048
         }
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public void set(int index, E data){ // changes the data at the index to inputted 'data'
-        if(index < 0 || index >= size){
+    public void set(int index, E data) { // changes the data at the index to inputted 'data'
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         } else {
             int count = 0;
@@ -43,33 +43,34 @@ public class LL<E> { // still needs to be changed to fit 2048
         }
     }
 
-    public void add(E data){ // allows us to add Nodes to the pre-existing LinkedList
+    public void add(E data) { // allows us to add Nodes to the pre-existing LinkedList
         Node<E> newNode = new Node<E>(data);
-        if(head == null){ // if LinkedList was empty before creating this Node^
+        if (head == null) { // if LinkedList was empty before creating this Node^
             head = newNode;
             tail = newNode;
-        } else{ // if LinkedList already had previous Nodes
+        } else { // if LinkedList already had previous Nodes
             tail.next = newNode; // next Node will be the tail
             tail = newNode; // LinkedList' tail IS newNode
         }
         size++;
     }
+
     // make special cases for first and last
     // set the "next" Node for the Node before 'index' equal to the one after 'index' and
     // Java deletes the Node on its own
-    public E remove(int index){ // points Node before 'index' to Node after 'index' and Java erases the Node at 'index' since there's nothing pointing to it
-        if(index < 0 || index >= size){ // ensures inputted 'index' is possible
+    public E remove(int index) { // points Node before 'index' to Node after 'index' and Java erases the Node at 'index' since there's nothing pointing to it
+        if (index < 0 || index >= size) { // ensures inputted 'index' is possible
             throw new IndexOutOfBoundsException();
         }
-        if(index == 0) { // if we're removing the first Node
+        if (index == 0) { // if we're removing the first Node
             Node<E> copy = head; // makes copy of Node at 'index'
             head = head.next; // erases Node
             return copy.data; // returns copy of data at 'index'
         }
-        if(index == size - 1){ // if we're removing the last Node
+        if (index == size - 1) { // if we're removing the last Node
             int count = 0;
             Node<E> current = head;
-            while(count < size - 2){
+            while (count < size - 2) {
                 current = current.next;
                 count++;
             } // current should be equal to the Node before the last Node
@@ -97,48 +98,61 @@ public class LL<E> { // still needs to be changed to fit 2048
         return copy.data; // returns data from the Node at 'index' before it was erased
     }
 
-    public E get(int index){ // returns the specified Node from the desired LinkedList
-        if(index < 0 || index >= size){
+    public E get(int index) { // returns the specified Node from the desired LinkedList
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         } else {
             int count = 0;
             Node<E> current = head;
-            while(count < index){
+            while (count < index) {
                 current = current.next;
                 count++;
             }
             return current.data;
         }
     }
-    public void clear () {
+
+    public void clear() {
         head = null; //head points to nothing
         tail = null; //tail points to nothing
         size = 0; //size is now 0
     }
 
 
-    private class Node<E>{ // defines what is inside a Node ('data', pointer to next Node)
+    private class Node<E> { // defines what is inside a Node ('data', pointer to next Node)
         E data; // stores the data
         Node<E> next; // stores the next Node
 
-        public Node(E data){ // create a Node with a specified value
+        public Node(E data) { // create a Node with a specified value
             this.data = data;
         }
 
-        public Node(){ // allows us to create an empty Node (no data, still has pointer)
+        public Node() { // allows us to create an empty Node (no data, still has pointer)
 
         }
 
-        public String toString(){ // toString for Nodes
+        public String toString() { // toString for Nodes
 
             return "" + data;
         }
     }
 
-    public String toString(){ // toString for LinkedList
+    public String toString() { // toString for LinkedList
         String str = "";
         // how to retrieve a specific Node's data?
         return str;
     }
 
+    public E[] toArray() {
+        Object[] arr = new Object[size];
+        Node<E> current = head;
+        int i = 0;
+        while (current != null) {
+            arr[i] = current.data;
+            current = current.next;
+            i++;
+        }
+        return (E[])arr;
+    }
 }
+
